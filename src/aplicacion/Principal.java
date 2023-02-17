f8d2c1415f042722e4ea22f49d9fa4f1eca834a5
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-17 09:14:26 +0100

    Ejercicio13 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import dominio.Ejercicio13;
+
+public class Principal {
+    public static void main(String[] args) {
+        double numero = (1 + (Math.sqrt(5))) / 2;
+
+        System.out.println(" " + Ejercicio13.equals(numero));
+    }
 }
--- src/dominio/Ejercicio13.java
+++ src/dominio/Ejercicio13.java
@@ -1,2 +1,25 @@
-package dominio;public class Ejercicio13 {
+package dominio;
+
+public class Ejercicio13 {
+    public static double fibonacci12(int n){
+        if(n<2){
+            return 0;
+        }else{
+            return fibonacci12(n-1)+fibonacci12(n-2);
+        }
+    }
+    public static double fibonacci13(int n){
+        if(n<2){
+            return 0;
+        }else{
+            return fibonacci13(n-1)+fibonacci13(n-2);
+        }
+    }
+    public static boolean equals(double numero){
+        if(fibonacci13(13)/fibonacci12(12)==numero){
+            return  true;
+        }else{
+            return false;
+        }
+    }
 }
